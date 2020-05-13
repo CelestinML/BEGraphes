6b8e3bf2f854a06ccc1ea0c855e1c5e9dd1ca105
CelestinML <celestin.moenne@gmail.com>
2020-05-13 18:19:24 +0200

    Mini fix labelstar

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/LabelStar.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/LabelStar.java
@@ -2,8 +2,6 @@ package org.insa.graphs.algorithm.shortestpath;
 import org.insa.graphs.algorithm.AbstractInputData;
 import org.insa.graphs.model.Node;
 
-//import org.insa.graphs.algorithm.shortestpath.ShortestPathData;
-
 public class LabelStar extends Label {
 	
 	private Node node_dest;
