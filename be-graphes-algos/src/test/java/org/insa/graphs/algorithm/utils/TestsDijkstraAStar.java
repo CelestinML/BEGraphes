ae48a00732e1247b3471eb92dbbaed9bf0831ddb
CelestinML <celestin.moenne@gmail.com>
2020-05-20 17:28:20 +0200

    Avancement tests Junit
    Comparaison Bellman faite + Vérification réaliste + Chemin impossible + Chemin vide
    Mais peut-être séparer tests optimalité et tests validité ?

--- be-graphes-algos/src/test/java/org/insa/graphs/algorithm/utils/TestsDijkstraAStar.java
+++ be-graphes-algos/src/test/java/org/insa/graphs/algorithm/utils/TestsDijkstraAStar.java
@@ -2,33 +2,26 @@ package org.insa.graphs.algorithm.utils;
 
 import static org.junit.Assert.*;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 
 import org.insa.graphs.algorithm.ArcInspector;
 import org.insa.graphs.algorithm.ArcInspectorFactory;
 import org.insa.graphs.algorithm.shortestpath.AStarAlgorithm;
 import org.insa.graphs.algorithm.shortestpath.BellmanFordAlgorithm;
 import org.insa.graphs.algorithm.shortestpath.DijkstraAlgorithm;
-import org.insa.graphs.algorithm.shortestpath.ShortestPathAlgorithm;
 import org.insa.graphs.algorithm.shortestpath.ShortestPathData;
+import org.insa.graphs.algorithm.shortestpath.ShortestPathSolution;
 import org.insa.graphs.model.Graph;
 import org.insa.graphs.model.Path;
 import org.insa.graphs.model.io.BinaryGraphReader;
-import org.insa.graphs.model.io.BinaryPathReader;
 import org.insa.graphs.model.io.GraphReader;
-import org.insa.graphs.model.io.PathReader;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TestOptimalite {
+public class TestsDijkstraAStar {
 	
     static Graph graph;
     static ArcInspector distance, temps;
@@ -39,11 +32,13 @@ public class TestOptimalite {
     static AStarAlgorithm aStarDistance, aStarTemps;
     static Path pathDijkstraDistance, pathDijkstraTemps, pathBellmanDistance, 
     		    pathBellmanTemps, pathAStarDistance, pathAStarTemps;
-	
+	static ShortestPathSolution solutionDijkstraTemps, solutionDijkstraDistance, solutionAStarTemps, 
+	                            solutionAStarDistance, solutionBellmanTemps, solutionBellmanDistance;
     static String insa = "C:\\Users\\celes\\Desktop\\BEGraphes\\BEGraphes\\Carte\\insa.mapgr";
     static String carre = "C:\\Users\\celes\\Desktop\\BEGraphes\\BEGraphes\\Carte\\carre.mapgr";
+    static String polynesie = "C:\\Users\\celes\\Desktop\\BEGraphes\\BEGraphes\\Carte\\french-polynesia.mapgr";
     
-    public static void initDijkstraDistance(String mapName) throws IOException {
+    public static void initDijkstraDistance(String mapName, int nodeOrigine, int nodeDest) throws IOException {
 	
 	    //Création du lecteur de graph
 	    final GraphReader reader = new BinaryGraphReader(
@@ -56,20 +51,23 @@ public class TestOptimalite {
 	    //Création des filtres
 	    distance = ArcInspectorFactory.getAllFilters().get(0);
 	    //Instanciation des données
-	    dataDistance = new ShortestPathData(graph, graph.get(0), graph.get(graph.size()-1), distance);
-	    distanceMin = graph.get(0).getPoint().distanceTo(graph.get(graph.size()-1).getPoint());
+	    dataDistance = new ShortestPathData(graph, graph.get(nodeOrigine), graph.get(nodeDest), distance);
+	    distanceMin = graph.get(nodeOrigine).getPoint().distanceTo(graph.get(nodeDest).getPoint());
 	    
 	    
 	    //Instanciation des algos
 	    dijkstraDistance = new DijkstraAlgorithm(dataDistance);
 	    bellmanDistance = new BellmanFordAlgorithm(dataDistance);
-	
+	    
+	    //Création des solutions
+	    solutionBellmanDistance = bellmanDistance.run();
+	    solutionDijkstraDistance = dijkstraDistance.run();
 	    //Création des paths
-	    pathDijkstraDistance = dijkstraDistance.run().getPath();
-	    pathBellmanDistance = bellmanDistance.run().getPath();
+	    pathDijkstraDistance = solutionDijkstraDistance.getPath();
+	    pathBellmanDistance = solutionBellmanDistance.getPath();
 	}
 	
-    public static void initDijkstraTemps(String mapName) throws IOException {
+    public static void initDijkstraTemps(String mapName, int nodeOrigine, int nodeDest) throws IOException {
 	
 	    //Création du lecteur de graph
 	    final GraphReader reader = new BinaryGraphReader(
@@ -82,19 +80,22 @@ public class TestOptimalite {
 	    //Création des filtres
 	    temps = ArcInspectorFactory.getAllFilters().get(2);
 	    //Instanciation des données
-	    dataTemps = new ShortestPathData(graph, graph.get(0), graph.get(graph.size()-1), temps);
-	    tempsMin = graph.get(0).getPoint().distanceTo(graph.get(graph.size()-1).getPoint())/(dataTemps.getGraph().getGraphInformation().getMaximumSpeed()/3.6);
+	    dataTemps = new ShortestPathData(graph, graph.get(nodeOrigine), graph.get(nodeDest), temps);
+	    tempsMin = graph.get(nodeOrigine).getPoint().distanceTo(graph.get(nodeDest).getPoint())/(dataTemps.getGraph().getGraphInformation().getMaximumSpeed()/3.6);
 	    
 	    //Instanciation des algos
 	    dijkstraTemps = new DijkstraAlgorithm(dataTemps);
 	    bellmanTemps = new BellmanFordAlgorithm(dataTemps);
 	
+	    //Création des solutions
+	    solutionBellmanTemps = bellmanTemps.run();
+	    solutionDijkstraTemps = dijkstraTemps.run();
 	    //Création des paths
-	    pathDijkstraTemps = dijkstraTemps.run().getPath();
-	    pathBellmanTemps = bellmanTemps.run().getPath();
+	    pathDijkstraTemps = solutionDijkstraTemps.getPath();
+	    pathBellmanTemps = solutionBellmanTemps.getPath();
 	}
 	
-    public static void initAStarDistance(String mapName) throws IOException {
+    public static void initAStarDistance(String mapName, int nodeOrigine, int nodeDest) throws IOException {
 	
 	    //Création du lecteur de graph
 	    final GraphReader reader = new BinaryGraphReader(
@@ -107,20 +108,23 @@ public class TestOptimalite {
 	    //Création des filtres
 	    distance = ArcInspectorFactory.getAllFilters().get(0);
 	    //Instanciation des données
-	    dataDistance = new ShortestPathData(graph, graph.get(0), graph.get(graph.size()-1), distance);
-	    distanceMin = graph.get(0).getPoint().distanceTo(graph.get(graph.size()-1).getPoint());
+	    dataDistance = new ShortestPathData(graph, graph.get(nodeOrigine), graph.get(nodeDest), distance);
+	    distanceMin = graph.get(nodeOrigine).getPoint().distanceTo(graph.get(nodeDest).getPoint());
 	    
 	    
 	    //Instanciation des algos
 	    bellmanDistance = new BellmanFordAlgorithm(dataDistance);
 	    aStarDistance = new AStarAlgorithm(dataDistance);
 	
+	    //Création des solutions
+	    solutionBellmanDistance = bellmanDistance.run();
+	    solutionAStarDistance = aStarDistance.run();
 	    //Création des paths
-	    pathBellmanDistance = bellmanDistance.run().getPath();
-	    pathAStarDistance = aStarDistance.run().getPath();
+	    pathBellmanDistance = solutionBellmanDistance.getPath();
+	    pathAStarDistance = solutionAStarDistance.getPath();
 	}
     
-    public static void initAStarTemps(String mapName) throws IOException {
+    public static void initAStarTemps(String mapName, int nodeOrigine, int nodeDest) throws IOException {
     	
 	    //Création du lecteur de graph
 	    final GraphReader reader = new BinaryGraphReader(
@@ -133,17 +137,20 @@ public class TestOptimalite {
 	    //Création des filtres
 	    temps = ArcInspectorFactory.getAllFilters().get(2);
 	    //Instanciation des données
-	    dataTemps = new ShortestPathData(graph, graph.get(0), graph.get(graph.size()-1), temps);
-	    tempsMin = graph.get(0).getPoint().distanceTo(graph.get(graph.size()-1).getPoint())/(dataTemps.getGraph().getGraphInformation().getMaximumSpeed()/3.6);
+	    dataTemps = new ShortestPathData(graph, graph.get(nodeOrigine), graph.get(nodeDest), temps);
+	    tempsMin = graph.get(nodeOrigine).getPoint().distanceTo(graph.get(nodeDest).getPoint())/(dataTemps.getGraph().getGraphInformation().getMaximumSpeed()/3.6);
 	    
 	    
 	    //Instanciation des algos
 	    bellmanTemps = new BellmanFordAlgorithm(dataTemps);
 	    aStarTemps = new AStarAlgorithm(dataTemps);
 	
+	    //Création des solutions
+	    solutionBellmanTemps = bellmanTemps.run();
+	    solutionAStarTemps = aStarTemps.run();
 	    //Création des paths
-	    pathBellmanTemps = bellmanTemps.run().getPath();
-	    pathAStarTemps = aStarTemps.run().getPath();
+	    pathBellmanTemps = solutionBellmanTemps.getPath();
+	    pathAStarTemps = solutionAStarTemps.getPath();
 	}
 	
 	//On ne peut pas comparer directement des floats ou des doubles
@@ -159,46 +166,92 @@ public class TestOptimalite {
 	}
 	
 	@Test
-	public void testDijkstraDistanceInsa() throws IOException {
-		initDijkstraDistance(insa);
+	public void comparaisonBellmanDijkstraDistanceInsa() throws IOException {
+		initDijkstraDistance(insa, 400, 900);
 		assertTrue(thresholdBasedFloatsComparison(pathDijkstraDistance.getLength(), pathBellmanDistance.getLength()));
 	}
 	@Test
-	public void testDijkstraTempsInsa() throws IOException {
-		initDijkstraTemps(insa);
+	public void comparaisonBellmanDijkstraTempsInsa() throws IOException {
+		initDijkstraTemps(insa, 400, 900);
 		assertTrue(thresholdBasedFloatsComparison(pathDijkstraTemps.getMinimumTravelTime(), pathBellmanTemps.getMinimumTravelTime()));
 	}
 	
 	@Test
-	public void testAStarDistanceInsa() throws IOException {
-		initAStarDistance(insa);
+	public void comparaisonBellmanAStarDistanceInsa() throws IOException {
+		initAStarDistance(insa, 400, 900);
 		assertTrue(thresholdBasedFloatsComparison(pathAStarDistance.getLength(), pathBellmanDistance.getLength()));
 	}
 	@Test
-	public void testAStarTempsInsa() throws IOException {
-		initAStarTemps(insa);
+	public void comparaisonBellmanAStarTempsInsa() throws IOException {
+		initAStarTemps(insa, 400, 900);
 		assertTrue(thresholdBasedFloatsComparison(pathAStarTemps.getMinimumTravelTime(), pathBellmanTemps.getMinimumTravelTime()));
 	}
 	
 	@Test
-	public void testDijkstraDistanceCarre() throws IOException {
-		initDijkstraDistance(carre);
+	public void comparaisonBellmanDijkstraDistanceCarre() throws IOException {
+		initDijkstraDistance(carre, 20, 15);
 		assertTrue(thresholdBasedFloatsComparison(pathDijkstraDistance.getLength(), pathBellmanDistance.getLength()));
 	}
 	@Test
-	public void testDijkstraTempsCarre() throws IOException {
-		initDijkstraTemps(carre);
+	public void comparaisonBellmanDijkstraTempsCarre() throws IOException {
+		initDijkstraTemps(carre, 20, 15);
 		assertTrue(thresholdBasedFloatsComparison(pathDijkstraTemps.getMinimumTravelTime(), pathBellmanTemps.getMinimumTravelTime()));
 	}
 	
 	@Test
-	public void testAStarDistanceCarre() throws IOException {
-		initAStarDistance(carre);
+	public void comparaisonBellmanAStarDistanceCarre() throws IOException {
+		initAStarDistance(carre, 20, 15);
 		assertTrue(thresholdBasedFloatsComparison(pathAStarDistance.getLength(), pathBellmanDistance.getLength()));
 	}
 	@Test
-	public void testAStarTempsCarre() throws IOException {
-		initAStarTemps(carre);
+	public void comparaisonBellmanAStarTempsCarre() throws IOException {
+		initAStarTemps(carre, 20, 15);
 		assertTrue(thresholdBasedFloatsComparison(pathAStarTemps.getMinimumTravelTime(), pathBellmanTemps.getMinimumTravelTime()));
+	}
+	
+	@Test
+	public void realisticTestAStar() throws IOException {
+		initAStarTemps(insa, 400, 900);
+		assert tempsMin < pathAStarTemps.getMinimumTravelTime();
+		initAStarDistance(insa, 400, 900);
+		assert distanceMin < pathAStarDistance.getLength();
+	}
+	@Test
+	public void realisticTestDijkstra() throws IOException {
+		initDijkstraTemps(insa, 400, 900);
+		assert tempsMin < pathDijkstraTemps.getMinimumTravelTime();
+		initDijkstraDistance(insa, 400, 900);
+		assert distanceMin < pathDijkstraDistance.getLength();
+	}
+	
+	@Test
+	public void impossiblePathDijkstra() throws IOException {
+		initDijkstraTemps(polynesie, 0, 24);
+		assertFalse(solutionDijkstraTemps.isFeasible());
+		initDijkstraDistance(polynesie, 0, 24);
+		assertFalse(solutionDijkstraDistance.isFeasible());
+	}
+	
+	@Test
+	public void impossiblePathAStar() throws IOException {
+		initAStarTemps(polynesie, 0, 24);
+		assertFalse(solutionAStarTemps.isFeasible());
+		initAStarDistance(polynesie, 0, 24);
+		assertFalse(solutionAStarDistance.isFeasible());
+	}
+	
+	@Test
+	public void nullPathDijkstra() throws IOException {
+		initDijkstraTemps(carre, 0, 0);
+		assertEquals(pathDijkstraTemps.size(), 0);
+		initDijkstraDistance(carre, 0, 0);
+		assertEquals(pathDijkstraDistance.size(), 0);
+	}
+	@Test
+	public void nullPathAStar() throws IOException {
+		initAStarTemps(carre, 0, 0);
+		assertEquals(pathAStarTemps.size(), 0);
+		initAStarDistance(carre, 0, 0);
+		assertEquals(pathAStarDistance.size(), 0);
 	}
 }
